6b9c9232c18165d55fb79da47f6007caf29acaae
Francis Eclevia <65243967+francisecleviaCI@users.noreply.github.com>
2020-06-23 13:40:29 -0400

    Submission

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,10 +1,17 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate = require('./calculate-back-end.js')
+
+let input1 = getInput(0)  //Number
+let input2 = getInput(1)  //Operator
+let input3 = getInput(2)  //Number
 
 
 
 
+let result = calculate(input1, input3, input2);
+console.log(result)
 
 
 /******************************************
