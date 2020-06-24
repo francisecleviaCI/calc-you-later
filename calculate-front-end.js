b1bfbd3a5b527409ac614d45d117665533cd7d6a
Francis Eclevia <franciseclevia@Franciss-iMac.local>
2020-06-24 09:45:21 -0400

    testing

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -6,13 +6,25 @@ const calculate = require('./calculate-back-end.js')
 let input1 = getInput(0)  //Number
 let input2 = getInput(1)  //Operator
 let input3 = getInput(2)  //Number
+let input4 = getInput(3)
 
-
+if (input3 === ''){
+  return input1 + input3;
+}
 
 
 let result = calculate(input1, input3, input2);
 console.log(result)
+//////
+// let input1 = getInput(0)  //Number
+// let input2 = getInput(1)  //Operator
+// let input3 = getInput(2)  //Number
 
+
+
+
+// let result = calculate(input1, input3, input2);
+// console.log(result)
 
 /******************************************
  * OUR CODE BELOW--DON'T TOUCH!           *
